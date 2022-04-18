60f80967b1d9b098eeabad00065b88d8e31fee8b
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-04-18 12:47:03 -0300

    feat: add perfil-photo and react-router-dom lib at version 5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { Route } from 'react-router-dom';
 
-import './App-dark.css';
+import './App.css';
 
 import Home from './pages/Home';
 import About from './pages/About';
-import Projects from './pages/Projects';
 
 function App() {
   return (
     <>
       <Route exact path="/" component={ Home } />
       <Route path="/about" component={ About } />
-      <Route path="/projects" component={ Projects } />
     </>
   );
 }
