da07bb0d1d3a9930e5d72c96555ca35481f63dc8
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-04-18 12:25:33 -0300

    feat: create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        <Link to="/" className="home hover">home</Link>
+        <Link to="/about" className="about hover">sobre</Link>
+      </>
+    );
+  }
+}
