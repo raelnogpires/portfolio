5e4cc0683b58f3f0be1708236eea139afbbb3bb3
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-06-09 15:38:42 -0300

    refactor: change stack to back-end

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,7 +18,7 @@ export default class About extends Component {
           <h2 id="hi">Olá, eu sou o Rael!</h2>
           <span id="about-text">
             Moro em São Sebastião/SP, aonde nasci e fui criado - um autêntico "caiçara". Aos 20 anos terminei uma graduação em Gestão Pública e decidi estudar sobre algo que sempre esteve ao meu redor e presente na minha vida, a Tecnologia.<br />
-            <br />Sou desenvolvedor Front-end, mas continuo trilhando meus estudos na Trybe para me tornar um desenvolvedor Web Full Stack. A área de Back-end me chama atenção e estou gostando de desenvolver projetos nesse ambiente.<br />
+            <br />Sou desenvolvedor back-end, mas continuo trilhando meus estudos na Trybe para me tornar um desenvolvedor Web Full Stack e ter conhecimento sobre ciência de dados.<br />
             <br />Sigo estudando e trabalhando para poder contribuir com a sociedade e ajudar o mundo a ser um lugar melhor!
           </span>
         </div>
