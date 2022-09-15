bf5a53d22b06b69c38cfca3b8310b56e11a7781c
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-09-15 13:03:01 -0300

    feat: changes in about me section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,9 +19,9 @@ export default class About extends Component {
         <div id="about-me">
           <h2 id="hi">Olá, eu sou o Rael!</h2>
           <span id="about-text">
-            Moro em São Sebastião/SP, aonde nasci e fui criado - um autêntico "caiçara". Aos 20 anos terminei uma graduação em Gestão Pública e decidi estudar sobre algo que sempre esteve ao meu redor e presente na minha vida, a Tecnologia.<br />
-            <br />Sou desenvolvedor back-end, mas continuo trilhando meus estudos na Trybe para me tornar um desenvolvedor Web Full Stack e ter conhecimento sobre ciência de dados.<br />
-            <br />Sigo estudando e trabalhando para poder contribuir com a sociedade e ajudar o mundo a ser um lugar melhor!
+          Moro em São Sebastião, litoral norte de São Paulo, município em que nasci e fui criado. Aos 20 anos terminei uma graduação de Tecnologia em Gestão Pública e decidir estudar sobre algo que sempre esteve ao meu redor e presente na minha vida, a Tecnologia.<br />
+          <br />Sou desenvolvedor Back-end com formação em Desenvolvimento Web Full-stack em andamento pela Trybe, com conhecimento técnico e experiência comprovada em desenvolvimento de projetos tanto no Front-end, quanto Back-end e com uma pitada de Ciência da Computação.<br />
+          <br />Sigo estudando e trabalhando para poder contribuir com a sociedade e ajudar o mundo a ser um lugar melhor!
           </span>
         </div>
         <div id="skills">
